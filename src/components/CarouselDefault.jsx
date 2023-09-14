8cd9e2499210dcd69f8fd981449f5f045b22020b
KeithPetr <keith.petryshyn@gmail.com>
2023-09-14 17:55:28 -0400

    Working autoplay on carousel

--- src/components/CarouselDefault.jsx
+++ src/components/CarouselDefault.jsx
@@ -1,26 +1,31 @@
+/* eslint-disable react/prop-types */
 import { Carousel } from "@material-tailwind/react";
-import Logo from "../assets/logo.jpg"
-import Test from "../assets/test.jpg"
-import Test2 from "../assets/test2.jpg"
+import Logo from "../assets/logo.jpg";
+import Test from "../assets/test.jpg";
+import Test2 from "../assets/test2.jpg";
 
 export default function CarouselDefault() {
-    return (
-        <Carousel className="w-1/2 h-1/2 mt-6">
-            <img 
+  const imageUrls = [Logo, Test, Test2];
+
+  return (
+    <Carousel
+      className="w-1/2 h-1/2 mt-6 z-10"
+      transition={{ duration: 1 }}
+      autoplay={true}
+      autoplayDelay={4000}
+      loop={true}
+    >
+      {imageUrls.map((imageUrl, index) => {
+        console.log(index);
+        return (
+          <img
+            key={index}
             alt="birdingsnaps logo"
-            src={Logo}
+            src={imageUrl}
             className="h-full w-full object-cover"
-            />
-            <img 
-            alt="birdingsnaps logo"
-            src={Test}
-            className="h-full w-full object-cover"
-            />
-            <img 
-            alt="birdingsnaps logo"
-            src={Test2}
-            className="h-full w-full object-cover"
-            />
-        </Carousel>
-    )
+          />
+        );
+      })}
+    </Carousel>
+  );
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,13 +5,17 @@ import { Button } from "@material-tailwind/react";
 import CarouselDefault from "./CarouselDefault";
 
 export default function LandingPage() {
+
   return (
-    <div className="flex flex-col justify-center items-center bg-blue-900 text-blue-300 h-screen">
-      <div className="flex flex-col items-center ">
+    <div
+      className="flex flex-col justify-center items-center bg-blue-900 text-blue-300 h-screen relative"
+    >
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+      <div className="flex flex-col items-center z-10">
         <div className="flex items-center text-6xl ">
           <h1 className="">birdingsnaps</h1>
           <img
-            className="w-32 h-32 rounded-full mx-4"
+            className="w-32 h-32 rounded-full mx-4 object-cover"
             src={Logo}
             alt="birding snaps logo"
           />
