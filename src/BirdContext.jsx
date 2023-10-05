6d33d9e2478386d83449e21bbb187b830cad0833
KeithPetr <keith.petryshyn@gmail.com>
2023-10-04 23:04:19 -0400

    able to add favorites to a Favorite component
    Able to keep track of what has been favorites so that they can't be favorited again

--- src/BirdContext.jsx
+++ src/BirdContext.jsx
@@ -1,29 +1,39 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
+import { auth } from "../firebase.config";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const BirdContext = createContext();
 
 export { BirdContext };
 
 export default function BirdProvider({ children }) {
+  const [user] = useAuthState(auth);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [searchTerms, setSearchTerms] = useState("");
   const [filteredBirds, setFilteredBirds] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [selectedBirdImage, setSelectedBirdImage] = useState(null);
   const [firstImageUrls, setFirstImageUrls] = useState(null);
-  const [getLetterResults, setGetLetterResults] = useState(false)
+  const [getLetterResults, setGetLetterResults] = useState(false);
   const [imageUrls, setImageUrls] = useState([]);
   const [matchingImages, setMatchingImages] = useState([]);
-  const [displayBirdDetails, setDisplayBirdDetails] = useState(false)
+  const [displayBirdDetails, setDisplayBirdDetails] = useState(false);
   const [selectedBirdName, setSelectedBirdName] = useState("");
-  const [showBirdGallery, setShowBirdGallery] = useState(false)
+  const [showBirdGallery, setShowBirdGallery] = useState(false);
   const [clickedImageUrl, setClickedImageUrl] = useState(null);
-  const [showLoginModal, setShowLoginModal] = useState(false)
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [imageFavorites, setImageFavorites] = useState([]);
+  const [showFavorites, setShowFavorites] = useState(false);
 
   return (
     <BirdContext.Provider
       value={{
+        showFavorites,
+        setShowFavorites,
+        user,
+        imageFavorites,
+        setImageFavorites,
         showLoginModal,
         setShowLoginModal,
         isModalVisible,
