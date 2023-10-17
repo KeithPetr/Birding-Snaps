92b90d3a00734178c0dc9bdaaf709f5f0052c5d1
KeithPetr <keith.petryshyn@gmail.com>
2023-10-16 23:52:01 -0400

    imported mountains jpg into the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 /* eslint-disable react/prop-types */
 import Home from "./components/Home";
+import Mountains from "./photos/mountains.jpg";
 
 export default function App() {
-  return <Home />;
+  const bodyStyles = {
+    backgroundImage: `url(${Mountains})`,
+    backgroundSize: 'cover',
+  };
+
+  return (
+    <div style={bodyStyles}>
+      <Home />
+    </div>
+  );
 }
